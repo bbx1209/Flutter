494d9c0a2a8e2cf53f72807c007710c8351061d8
bbx1209 <0weibo0@163.com>
2021-06-14 13:15:40 +0800

    flutter row

---
+++ flutter_news_app/lib/Rows.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class RowRoute extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: "Row flow  test",
+      ),d
+       body: Center(
+         child: Text("This is a row test view"),
+       ),
+    );
+  }
+}
