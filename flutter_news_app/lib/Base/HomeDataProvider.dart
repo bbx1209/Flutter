bff31313538577fb85d747ad41e642f3cf556f7b
bbx1209 <0weibo0@163.com>
2021-09-25 12:14:08 +0800

    dart language

--- flutter_news_app/lib/Base/HomeDataProvider.dart
+++ flutter_news_app/lib/Base/HomeDataProvider.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter_news_app/Base/RoutParam.dart';
 import 'package:flutter_news_app/Section/Async/AsyncRoute.dart';
+import 'package:flutter_news_app/Section/DartLanguage/DartLanguageRoute.dart';
 import 'package:flutter_news_app/Section/Interface/ConstValue.dart';
 import 'package:flutter_news_app/Section/Merterial/Merterial.dart';
 
@@ -13,7 +14,7 @@ class HomeDataProvider {
       RouteItem("列表", ListViewRoute()),
       RouteItem("页面元素", MerterialRoute()),
       RouteItem("Async", AsyncRoute()),
-
+      RouteItem("Dart Language ", DartLanguageRoute()),
     ];
     return items;
   }
--- flutter_news_app/lib/Section/Async/AsyncRoute.dart
+++ flutter_news_app/lib/Section/Async/AsyncRoute.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:toast/toast.dart';
 
 
 const oneSecond = Duration(seconds: 1);
@@ -12,6 +13,21 @@ class AsyncRoute extends StatefulWidget {
 }
 
 class _AsyncRouteState extends State<AsyncRoute> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    prinWithDelay("delay one second print");
+  }
+
+  Future<void> prinWithDelay(String message) async {
+    await Future.delayed(oneSecond);
+    print(message);
+    Toast.show(message, this.context);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
