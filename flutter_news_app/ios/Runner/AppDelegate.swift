44efec869aec8cf2e9cf752c724cb2e4a35a2d12
bbx1209 <0weibo0@163.com>
2021-09-27 17:45:50 +0800

    ios 插件部分代码

--- flutter_news_app/ios/Runner/AppDelegate.swift
+++ flutter_news_app/ios/Runner/AppDelegate.swift
@@ -8,16 +8,27 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-      let  controller : FlutterViewController = application.keyWindow?.rootViewController as! FlutterViewController
-      let  methodChannel : FlutterMethodChannel =  FlutterMethodChannel.init(name: "helloworld.flutter.io/info", binaryMessenger: controller as! FlutterBinaryMessenger)
       
-      methodChannel.setMethodCallHandler ({(call : FlutterMethodCall, result:FlutterResult) -> Void in
-          if("getInfomationFromNative" == call.method) {
-              getInfo(result: result)
-          }else {
-              result(FlutterMethodNotImplemented)
-          }
-      })
+      if #available(iOS 13.0, *) {
+          let scenes : Set<UIScene> =  application.connectedScenes
+          let scene = scenes.first
+          print("")
+      } else {
+          // Fallback on earlier versions
+      }
+
+      if let window = application.keyWindow {
+          let controller : FlutterViewController = window.rootViewController as! FlutterViewController
+          let  methodChannel : FlutterMethodChannel =  FlutterMethodChannel.init(name: "helloworld.flutter.io/info", binaryMessenger: controller as! FlutterBinaryMessenger)
+          methodChannel.setMethodCallHandler ({(call : FlutterMethodCall, result:FlutterResult) -> Void in
+              if("getInfomationFromNative" == call.method) {
+                  getInfo(result: result)
+              }else {
+                  result(FlutterMethodNotImplemented)
+              }
+          })
+      }
+      
       
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
