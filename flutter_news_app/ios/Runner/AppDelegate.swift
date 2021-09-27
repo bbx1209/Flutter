675108e521c836b40b6f255c5e21aa9a8032a9f4
bbx1209 <0weibo0@163.com>
2021-09-27 16:45:48 +0800

    iOS原生方法

--- flutter_news_app/ios/Runner/AppDelegate.swift
+++ flutter_news_app/ios/Runner/AppDelegate.swift
@@ -8,6 +8,28 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      let  controller : FlutterViewController = application.keyWindow?.rootViewController as! FlutterViewController
+      let  methodChannel : FlutterMethodChannel =  FlutterMethodChannel.init(name: "helloworld.flutter.io/info", binaryMessenger: controller as! FlutterBinaryMessenger)
+      
+      methodChannel.setMethodCallHandler ({(call : FlutterMethodCall, result:FlutterResult) -> Void in
+          if("getInfomationFromNative" == call.method) {
+              getInfo(result: result)
+          }else {
+              result(FlutterMethodNotImplemented)
+          }
+      })
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+private func getInfo(result :  FlutterResult) {
+    let device = UIDevice.current
+    device.isBatteryMonitoringEnabled = true
+    if(device.batteryState == UIDevice.BatteryState.unknown) {
+        result("there is an error")
+    } else {
+        result("value from iOS")
+    }
+}
+
