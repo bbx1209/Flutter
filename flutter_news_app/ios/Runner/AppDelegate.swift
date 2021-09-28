00958ba7b623597459e73b3b94ccb59b7daf5444
bbx1209 <0weibo0@163.com>
2021-09-28 10:41:43 +0800

    插件iOS部分

--- flutter_news_app/ios/Runner/AppDelegate.swift
+++ flutter_news_app/ios/Runner/AppDelegate.swift
@@ -10,24 +10,22 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
       
       if #available(iOS 13.0, *) {
-          let scenes : Set<UIScene> =  application.connectedScenes
-          let scene = scenes.first
-          print("")
+          if let window = self.window {
+              let controller : FlutterViewController = window.rootViewController as! FlutterViewController
+              let  methodChannel : FlutterMethodChannel =  FlutterMethodChannel.init(name: "helloworld.flutter.io/info", binaryMessenger: controller as! FlutterBinaryMessenger)
+              methodChannel.setMethodCallHandler ({(call : FlutterMethodCall, result:FlutterResult) -> Void in
+                  if("getInfomationFromNative" == call.method) {
+                      getInfo(result: result)
+                  }else {
+                      result(FlutterMethodNotImplemented)
+                  }
+              })
+          }
       } else {
           // Fallback on earlier versions
       }
 
-      if let window = application.keyWindow {
-          let controller : FlutterViewController = window.rootViewController as! FlutterViewController
-          let  methodChannel : FlutterMethodChannel =  FlutterMethodChannel.init(name: "helloworld.flutter.io/info", binaryMessenger: controller as! FlutterBinaryMessenger)
-          methodChannel.setMethodCallHandler ({(call : FlutterMethodCall, result:FlutterResult) -> Void in
-              if("getInfomationFromNative" == call.method) {
-                  getInfo(result: result)
-              }else {
-                  result(FlutterMethodNotImplemented)
-              }
-          })
-      }
+    
       
       
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
@@ -35,12 +33,24 @@ import Flutter
 }
 
 private func getInfo(result :  FlutterResult) {
-    let device = UIDevice.current
-    device.isBatteryMonitoringEnabled = true
-    if(device.batteryState == UIDevice.BatteryState.unknown) {
-        result("there is an error")
-    } else {
+//    let device = UIDevice.current
+//    device.isBatteryMonitoringEnabled = true
+//    if(device.batteryState == UIDevice.BatteryState.unknown) {
+//        result("there is an error")
+//    } else {
         result("value from iOS")
+//    }
+}
+
+
+extension UIApplication {
+    @available(iOS 13.0, *)
+    var keyWindow: UIWindow? {
+        let activeScenes =  self.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+        let windowScenes = activeScenes.compactMap { $0 as? UIWindowScene }
+        return  windowScenes.first?.windows
+            .filter { $0.isKeyWindow }.first
     }
 }
 
--- flutter_news_app/lib/Section/MethodChannel/MethodChannelRoute.dart
+++ flutter_news_app/lib/Section/MethodChannel/MethodChannelRoute.dart
@@ -26,7 +26,7 @@ class _MethodChannelRouteState extends State<MethodChannelRoute> {
                 onPressed: () {
                   setState(() {
                     //更新文本框
-                    _infoFromNative = "message from native";
+                    // _infoFromNative = "message from native";
                     _getInfomationFromNative();
                   });
                 },
@@ -40,6 +40,6 @@ class _MethodChannelRouteState extends State<MethodChannelRoute> {
   Future<Null> _getInfomationFromNative() async {
     String info;
     String result = await platform.invokeMethod('getInfomationFromNative');
-    info = result;
+    _infoFromNative = result;
   }
 }
