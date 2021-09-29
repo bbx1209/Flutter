975a139a568ea51eee3e2ec667935969c83051c8
bbx1209 <0weibo0@163.com>
2021-09-29 09:40:47 +0800

    method channel ios part

--- flutter_news_app/ios/Runner/AppDelegate.m
+++ flutter_news_app/ios/Runner/AppDelegate.m
@@ -8,7 +8,12 @@
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
 
-
+    FlutterMethodChannel *methodChannel = [FlutterMethodChannel methodChannelWithName:@"helloworld.flutter.io/info" binaryMessenger:((FlutterViewController *)self.window.rootViewController).binaryMessenger];
+    [methodChannel setMethodCallHandler:^(FlutterMethodCall * _Nonnull call, FlutterResult  _Nonnull result) {
+        if ([call.method isEqualToString:@"getInfomationFromNative"]) {
+            result(@"value from iOS");
+        }
+    }];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
