a8c2c8381a0351fcf719ac2ee3d5bf33ca955d20
bbx1209 <0weibo0@163.com>
2021-10-02 08:46:33 +0800

    image issue

--- flutter_news_app/lib/Section/Merterial/ImageRoute.dart
+++ flutter_news_app/lib/Section/Merterial/ImageRoute.dart
@@ -5,80 +5,80 @@ class ImageRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var _image = AssetImage("images/walk1");
+    var _image = AssetImage("images/walk1.png");
     return Scaffold(
         appBar: AppBar(
           title: Text("ImageRoute"),
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image(
-                  image: _image,
-                  height: 50,
-                  width: 100,
-                  fit: BoxFit.fill,
-                ),
-                Image(
-                  image: _image,
-                  height: 50,
-                  width: 50,
-                  fit: BoxFit.contain,
-                ),
-                Image(
-                  image: _image,
-                  height: 50,
-                  width: 100,
-                  fit: BoxFit.cover,
-                ),
-                Image(
-                  image: _image,
-                  height: 50,
-                  width: 100,
-                  fit: BoxFit.fitHeight,
-                ),
-                Image(
-                  image: _image,
-                  height: 50,
-                  width: 100,
-                  fit: BoxFit.fitWidth,
-                ),
-                Image(
-                  image: _image,
-                  height: 50,
-                  width: 100,
-                  fit: BoxFit.scaleDown,
-                ),
-                Image(
-                  image: _image,
-                  width: 100,
-                  color: Colors.blue,
-                  colorBlendMode: BlendMode.difference,
-                  repeat: ImageRepeat.repeatY,
-                ),
-                Image(
-                  image: _image,
-                  height: 200,
-                  width: 100,
-                  fit: BoxFit.fill,
-                ),
-              ].map((e) {
-                return Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: SizedBox(
-                        width: 100,
-                        child: e,
-                      ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image(
+                image: _image,
+                height: 50,
+                width: 100,
+                fit: BoxFit.fill,
+              ),
+              Image(
+                image: _image,
+                height: 50,
+                width: 50,
+                fit: BoxFit.contain,
+              ),
+              Image(
+                image: _image,
+                height: 50,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+              Image(
+                image: _image,
+                height: 50,
+                width: 100,
+                fit: BoxFit.fitHeight,
+              ),
+              Image(
+                image: _image,
+                height: 50,
+                width: 100,
+                fit: BoxFit.fitWidth,
+              ),
+              Image(
+                image: _image,
+                height: 50,
+                width: 100,
+                fit: BoxFit.scaleDown,
+              ),
+              Image(
+                image: _image,
+                width: 100,
+                color: Colors.blue,
+                colorBlendMode: BlendMode.difference,
+                repeat: ImageRepeat.repeatY,
+              ),
+              Image(
+                image: _image,
+                height: 200,
+                width: 100,
+                fit: BoxFit.fill,
+              ),
+            ].map((e) {
+              return Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      width: 100,
+                      child: e,
                     ),
-                    Text(e.fit.toString())
-                  ],
-                );
-              }).toList(),
-            ),
+                  ),
+                  Text(e.fit.toString())
+                ],
+              );
+            }).toList(),
           ),
-        ));
+        )
+
+    );
   }
 }
