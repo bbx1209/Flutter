80e4c70eca5078980da73fb428c56ae962d97c3d
bbx1209 <0weibo0@163.com>
2021-10-09 15:17:49 +0800

    scale gesture

---
+++ flutter_news_app/lib/Section/Gesture/ScaleGestureRoute.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+class ScaleGestureRoute extends StatefulWidget {
+  const ScaleGestureRoute({Key key}) : super(key: key);
+
+  @override
+  _ScaleGestureRouteState createState() => _ScaleGestureRouteState();
+}
+
+class _ScaleGestureRouteState extends State<ScaleGestureRoute> {
+  double _width = 200.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("MoveGestureRoute"),
+        ),
+        body: Center(
+          child: GestureDetector(
+            child: Image.asset("images/mao.png", width: _width),
+            onScaleUpdate: (ScaleUpdateDetails e){
+              setState(() {
+                _width=200*e.scale.clamp(.1, 20.0);
+              });
+            },
+          ),
+        )
+    );
+  }
+}
