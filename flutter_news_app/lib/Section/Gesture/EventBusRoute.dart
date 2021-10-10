d8652fcd1e9a358a7e8441e885f631579f7cc253
bbx1209 <0weibo0@163.com>
2021-10-10 11:17:59 +0800

    事件总线

--- flutter_news_app/lib/Section/Gesture/EventBusRoute.dart
+++ flutter_news_app/lib/Section/Gesture/EventBusRoute.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'EventBus.dart';
 
 class EventBusRoute extends StatefulWidget {
   const EventBusRoute({Key key}) : super(key: key);
@@ -10,60 +11,60 @@ class EventBusRoute extends StatefulWidget {
 class _EventBusRouteState extends State<EventBusRoute> {
   @override
   Widget build(BuildContext context) {
+
+    bus.on("print", (arg) {
+      print("receive note");
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text("EventBusRoute"),
       ),
       body: Center(
         child: Column(
-          children: [],
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 65),
+              child: TextButton(
+                child: Text("下个页面"),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return EventBusRoute2();
+                  }));
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
   }
 }
 
-typedef void EventCallBack(arg);
+class EventBusRoute2 extends StatelessWidget {
+  const EventBusRoute2({Key key}) : super(key: key);
 
-class EventBus {
-  //私有构造函数
-  EventBus._internal();
-
-  //单例模式
-  static EventBus _singleton = EventBus._internal();
-
-  //工厂构造函数
-  factory EventBus() => _singleton;
-
-  //保存时间订阅者。key 为时间名称， value为时间订阅者队列
-  final _emap = Map<Object, List<EventCallBack>>();
-
-  // 添加订阅者
-  void on(eventname, EventCallBack f) {
-    _emap[eventname] ??= <EventCallBack>[];
-    _emap[eventname].add(f);
-  }
-
-//移除订阅者
-
-  void off(eventname, [EventCallBack f]) {
-    var list = _emap[eventname];
-    if (eventname == null || list == null) return;
-    if (f == null) {
-      _emap[eventname] = null;
-    } else {
-      list.remove(f);
-    }
-  }
-
-//触发事件
-  void emit(eventname, [arg]) {
-    var list = _emap[eventname];
-    if (list == null) return;
-    int len = list.length - 1;
-    //反向遍历
-    for (var i = len; i > -1; --i) {
-      list[i](arg);
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("EventBusRoute2"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 65),
+              child: TextButton(
+                child: Text("发送广播"),
+                onPressed: () {
+                  bus.emit("print");
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
