143cf1071a06e629f54793625803e3a4ef002ef1
bbx1209 <0weibo0@163.com>
2021-10-10 10:24:50 +0800

    event bus

---
+++ flutter_news_app/lib/Section/Gesture/EventBusRoute.dart
@@ -0,0 +1,29 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class EventBusRoute extends StatefulWidget {
+  const EventBusRoute({Key key}) : super(key: key);
+
+  @override
+  _EventBusRouteState createState() => _EventBusRouteState();
+}
+
+class _EventBusRouteState extends State<EventBusRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("EventBusRoute"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
