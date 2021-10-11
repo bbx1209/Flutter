8b629d676d67699a8ef0bc0613acc148b5ff5dd3
bbx1209 <0weibo0@163.com>
2021-10-11 17:31:55 +0800

    自定义通知

--- flutter_news_app/lib/Section/Gesture/CustomNotificationRoute.dart
+++ flutter_news_app/lib/Section/Gesture/CustomNotificationRoute.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/Section/Gesture/MyNoticication.dart';
 
 class CustonNotifyRoute extends StatefulWidget {
   const CustonNotifyRoute({Key key}) : super(key: key);
@@ -10,14 +11,35 @@ class CustonNotifyRoute extends StatefulWidget {
 }
 
 class _CustonNotifyRouteState extends State<CustonNotifyRoute> {
+  String _msg = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("CustonNotifyRoute"),
       ),
-      body: Center(
-
+      body: NotificationListener<MyNotification>(
+        onNotification: (notification){
+          setState(() {
+            _msg+=notification.msg+"  ";
+          });
+          return true;
+        },
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Builder(
+                builder: (cnt){
+                  return ElevatedButton(onPressed: (){
+                    MyNotification("one notification").dispatch(cnt);
+                  }, child: Text("send Notofication"));
+                },
+              ),
+              Text(_msg)
+            ],
+          ),
+        ),
       ),
     );
   }
