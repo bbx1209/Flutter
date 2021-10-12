072dfcc58daad7aaa1ee6655cdeef13036cbaadb
bbx1209 <0weibo0@163.com>
2021-10-12 14:43:21 +0800

    stream builder

--- flutter_news_app/lib/Section/FutureBuilder/FutureBuliderRoute.dart
+++ flutter_news_app/lib/Section/FutureBuilder/FutureBuliderRoute.dart
@@ -15,25 +15,52 @@ class _FutureBuliderRouteState extends State<FutureBuliderRoute> {
         title: Text("FutureBuliderRoute"),
       ),
       body: Center(
-        child: FutureBuilder<String>(
-          future: mockNetworkData(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Text("Error: ${snapshot.error}");
+          child: Column(
+        children: [
+          FutureBuilder<String>(
+            future: mockNetworkData(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Text("Error: ${snapshot.error}");
+                } else {
+                  return Text("contents: ${snapshot.data}");
+                }
               } else {
-                return Text("contents: ${snapshot.data}");
+                return CircularProgressIndicator();
               }
-            } else {
-              return CircularProgressIndicator();
-            }
-          },
-        ),
-      ),
+            },
+          ),
+          StreamBuilder(
+              stream: counter(),
+              builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                if (snapshot.hasError) {
+                  return Text("Error: ${snapshot.error}");
+                }
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return Text("没有 stream");
+                  case ConnectionState.waiting:
+                    return Text("等待数据");
+                  case ConnectionState.active:
+                    return Text("actice : ${snapshot.data}");
+                  case ConnectionState.done:
+                    return Text("stream 已关闭");
+                }
+                return null;
+              })
+        ],
+      )),
     );
   }
 
   Future<String> mockNetworkData() async {
     return Future.delayed(Duration(seconds: 2), () => "来自网络的数据");
+  }
+
+  Stream<int> counter() {
+    return Stream.periodic(Duration(seconds: 1), (i) {
+      return i;
+    });
   }
 }
