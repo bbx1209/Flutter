b08f4e4dbf41ca73dc720cd30cd92e91d59b5ac9
bbx1209 <0weibo0@163.com>
2021-10-15 17:09:57 +0800

    dialog

--- flutter_news_app/lib/Section/Merterial/DialogRoute.dart
+++ flutter_news_app/lib/Section/Merterial/DialogRoute.dart
@@ -22,7 +22,22 @@ class _MyDialogRouteState extends State<MyDialogRoute> {
                   bool? result = await showDialog1(context);
                   print(result == null ? "取消" : "确认");
                 },
-                child: Text("Dialog"))
+                child: Text("Dialog")),
+            TextButton(
+                onPressed: () {
+                  simpleDialog(context);
+                },
+                child: Text("SimpleDialog")),
+            TextButton(
+                onPressed: () {
+                  showListDialog(context);
+                },
+                child: Text("ListDialog")),
+            TextButton(
+                onPressed: () {
+                  showUnconstrainsDialog(context);
+                },
+                child: Text("showUnconstrainsDialog")),
           ],
         ),
       ),
@@ -34,7 +49,8 @@ class _MyDialogRouteState extends State<MyDialogRoute> {
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text("Title", textAlign: TextAlign.center, style: TextStyle(color: Colors.red),),
+            title: Text("Title", textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.red),),
             content: Text("msg content", textAlign: TextAlign.center,),
             actions: [
               TextButton(
@@ -46,9 +62,108 @@ class _MyDialogRouteState extends State<MyDialogRoute> {
                   onPressed: () {
                     Navigator.of(context).pop(true);
                   },
-                  child: Text("确认", style:TextStyle(fontWeight: FontWeight.w700)))
+                  child: Text(
+                      "确认", style: TextStyle(fontWeight: FontWeight.w700)))
             ],
           );
         });
   }
+
+  Future<void> simpleDialog(BuildContext context) async {
+    int? i = await showDialog<int>(context: context, builder: (context) {
+      return SimpleDialog(
+        title: Text("选择吧"),
+        children: [
+          SimpleDialogOption(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text("英语"),
+            ),
+            onPressed: () {
+              Navigator.pop(context, 1);
+            },
+          ),
+          SimpleDialogOption(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text("中文"),
+            ),
+            onPressed: () {
+              Navigator.pop(context, 2);
+            },
+          )
+        ],
+      )
+    });
+    if (i != null) {
+      print("${i == 1 ? "英文" : "中文"}");
+    }
+  }
+
+  Future<void> showListDialog(BuildContext context) async {
+    int? index = await showDialog(context: context, builder: (context) {
+      return Dialog(
+        child: Column(
+          children: [
+            ListTile(title: Text("请选择"),),
+            Expanded(child: ListView.builder(
+              itemCount: 30,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text("${index}",
+                  ), onTap: () {
+                  Navigator.of(context).pop(index);
+                },
+                );
+              },
+
+            ),
+
+            )
+          ],
+        ),
+      );
+    });
+    if (index != null) {
+      print("点击了${index}");
+    }
+  }
+
+  Future<void> showUnconstrainsDialog(BuildContext context) async {
+    int? index = await showDialog(context: context, builder: (context) {
+      var child = Column(
+        children: [
+          ListTile(title: Text("请选择"),),
+          Expanded(child: ListView.builder(
+            itemCount: 30,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text("${index}",
+                ), onTap: () {
+                Navigator.of(context).pop(index);
+              },
+              );
+            },
+
+          ),
+
+          )
+        ],
+      );
+      return UnconstrainedBox(
+        constrainedAxis: Axis.vertical,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: 100, maxWidth: 200),
+          child: Material(
+            child: child,
+            type: MaterialType.card,
+          ),
+        ),
+      );
+    });
+    if (index != null) {
+      print("点击了${index}");
+    }
+  }
+
 }
