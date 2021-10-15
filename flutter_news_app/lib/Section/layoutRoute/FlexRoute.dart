d5357eaa26992c3cb65cd52045f9992e0100620f
bbx1209 <0weibo0@163.com>
2021-10-15 09:49:08 +0800

    flex with expanded

--- flutter_news_app/lib/Section/layoutRoute/FlexRoute.dart
+++ flutter_news_app/lib/Section/layoutRoute/FlexRoute.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class FlexRoute extends StatefulWidget {
@@ -12,6 +10,55 @@ class FlexRoute extends StatefulWidget {
 class _FlexRouteState extends State<FlexRoute> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("FlexRoute"),
+        ),
+        body: Column(
+          children: [
+            Flex(
+              direction: Axis.horizontal,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 30,
+                    color: Colors.blue,
+                  ),
+                  flex: 1,
+                ),
+                Expanded(
+                  child: Container(
+                    height: 30,
+                    color: Colors.red,
+                  ),
+                  flex: 1,
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: SizedBox(
+                height: 100,
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.green,
+                      ),
+                      flex: 2,
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Expanded(child: Container(color: Colors.red,),
+                    flex: 1,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
