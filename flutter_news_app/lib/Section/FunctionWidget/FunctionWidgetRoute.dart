c2316b2805f7576ef927ae41e1b4cdc2973ed519
bbx1209 <0weibo0@163.com>
2021-10-17 10:13:52 +0800

    function widget route

---
+++ flutter_news_app/lib/Section/FunctionWidget/FunctionWidgetRoute.dart
@@ -0,0 +1,76 @@
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/Base/RoutParam.dart';
+
+List<RouteItem> getMertialItems() {
+  List<RouteItem> items = [
+    // RouteItem("FlexRoute", FlexRoute()),
+
+
+
+  ];
+  return items;
+}
+
+class FunctionWidgetRoute extends StatefulWidget {
+  FunctionWidgetRoute({Key? key}) : super(key: key);
+
+  @override
+  _FunctionWidgetRouteState createState() => _FunctionWidgetRouteState();
+}
+
+class _FunctionWidgetRouteState extends State<FunctionWidgetRoute> {
+  List<RouteItem> items = [];
+  @override
+  void initState() {
+    super.initState();
+    items = getMertialItems();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FunctionWidgetRoute"),
+      ),
+      body: Center(
+          child: ListView.separated(
+            itemBuilder: (BuildContext buildContext, int index) {
+              var item = items[index];
+              return getItem(context, index, item);
+            },
+            separatorBuilder: (BuildContext buildContext, int index) {
+              return Padding(padding: EdgeInsets.only(left: 25),
+                child: Divider(color: Colors.black38,),);
+            },
+            itemCount: items.length,
+          )
+      ),
+    );
+  }
+
+
+  Widget getItem(BuildContext context, int index, RouteItem item) {
+    return GestureDetector(
+      child: Container(
+        child: Center(
+          child: Text("${item.title}"),
+        ),
+        height: 45,
+        color: Colors.cyan,
+      ),
+      onTap:(){
+        onClickItem(context,item);
+      },
+    );
+  }
+
+  void onClickItem(BuildContext context,RouteItem item) {
+    // Toast.show("点击了 ${item.title} ", context);
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return item.newRoute;
+    }));
+  }
+}
