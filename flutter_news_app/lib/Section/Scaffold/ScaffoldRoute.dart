2990f87153501eceb5b93a2f91305becea082f2a
bbx1209 <0weibo0@163.com>
2021-10-18 10:03:45 +0800

    scaffold drawer and bottom nacigator

--- flutter_news_app/lib/Section/Scaffold/ScaffoldRoute.dart
+++ flutter_news_app/lib/Section/Scaffold/ScaffoldRoute.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ScaffoldRoute extends StatefulWidget {
@@ -9,12 +9,68 @@ class ScaffoldRoute extends StatefulWidget {
 }
 
 class _ScaffoldRouteState extends State<ScaffoldRoute> {
+  int _selected =1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("ScaffoldRoute"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.share))
+        ],
       ),
+      drawer: MyDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+        ],
+        currentIndex: _selected,
+        onTap: (index){
+          setState(() {
+            _selected = index;
+          });
+        },
+        selectedItemColor: Colors.red,
+      ),
+    );
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: MediaQuery.removePadding(context: context,
+          removeTop: true,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(padding: EdgeInsets.only(top: 70, left: 25),
+                child: Row(
+                   children: [
+                     Padding(padding: EdgeInsets.symmetric(horizontal: 16),
+                       child: ClipOval(
+                           child: Image.asset("images/mao.png", width: 80,)
+                       ),),
+                     Text("name", style: TextStyle(fontWeight: FontWeight.bold),)
+                   ],
+                ),
+              ),
+
+              Expanded(child: ListView(
+                children: [
+                  ListTile(title: const Text("add accout"),leading: const Icon(Icons.add),),
+                  ListTile(title: const Text("settings"),leading: const Icon(Icons.settings),)
+                ],
+              ))
+
+
+            ],
+          )),
     );
   }
 }
