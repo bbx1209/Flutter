f93dbda2aee761f76a50c638cdfe00c8e133ba47
bbx1209 <0weibo0@163.com>
2021-10-20 10:54:25 +0800

    theme and color transform from 0x

--- flutter_news_app/lib/Section/Theme/ThemeRoute.dart
+++ flutter_news_app/lib/Section/Theme/ThemeRoute.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class ThemeRoute extends StatefulWidget {
@@ -10,19 +8,78 @@ class ThemeRoute extends StatefulWidget {
 }
 
 class _ThemeRouteState extends State<ThemeRoute> {
+  Color _themeColor = Colors.teal;
+  
+  Color c1 = Color(int.parse("dc380d", radix: 16)|0xFF000000);
+  Color c2 = Color(int.parse("ee22e0",radix: 16)).withAlpha(255);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ThemeRoute"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
+    ThemeData themeData = Theme.of(context);
+    return Theme(
+        data: ThemeData(
+            primaryColor: _themeColor,
+            iconTheme: IconThemeData(color: _themeColor)),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("ThemeRoute"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.favorite),
+                    Icon(Icons.airport_shuttle),
+                    Text("颜色跟做主题"),
+                  ],
+                ),
 
-          ],
-        ),
-      ),
-    );
+               Theme(
+                 data: themeData.copyWith(
+                   iconTheme: themeData.iconTheme.copyWith(
+                      color: Colors.black
+                   )
+                 ),
+                 child:  Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     Icon(Icons.favorite),
+                     Icon(Icons.airport_shuttle),
+                     Text("颜色固定"),
+                   ],
+                 ),
+               ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: c1,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      width: 10,
+                    ),
+                    Container(
+                      color: c2,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: TextButton(
+            child: Text("改变主题"),
+            onPressed: (){
+              setState(() {
+               _themeColor=  (_themeColor == Colors.teal ? Colors.blue : Colors.teal);
+              });
+            },
+          ),
+        ));
   }
 }
