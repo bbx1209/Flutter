29e89764a95ba7d10f3b30cabb816575267435c4
bbx1209 <0weibo0@163.com>
2021-10-22 15:38:36 +0800

    animation builder的优化

--- flutter_news_app/lib/Section/Animation/BaseAnimationRoute.dart
+++ flutter_news_app/lib/Section/Animation/BaseAnimationRoute.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class BaseAnimationRoute extends StatefulWidget {
@@ -8,13 +7,16 @@ class BaseAnimationRoute extends StatefulWidget {
   _BaseAnimationRouteState createState() => _BaseAnimationRouteState();
 }
 
-class _BaseAnimationRouteState extends State<BaseAnimationRoute> with SingleTickerProviderStateMixin {
+class _BaseAnimationRouteState extends State<BaseAnimationRoute>
+    with SingleTickerProviderStateMixin {
   late Animation<double> animation;
   late AnimationController controller;
+
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
     //使用弹性曲线
     animation = CurvedAnimation(parent: controller, curve: Curves.bounceInOut);
     //匀速
@@ -26,9 +28,9 @@ class _BaseAnimationRouteState extends State<BaseAnimationRoute> with SingleTick
     //用 animationwidget 简化后
     animation = Tween(begin: 0.0, end: 300.0).animate(controller);
 
-
     controller.forward();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +40,25 @@ class _BaseAnimationRouteState extends State<BaseAnimationRoute> with SingleTick
       body: Center(
         // child: Image.asset("images/mao.png",width: animation.value,height: animation.value,),
         //用 animationwidget 简化后
-        child: AnimationImage(animation: animation),
+        // child: AnimationImage(animation: animation),
+        //使用builder
+        child: AnimatedBuilder(
+          animation: animation,
+          child: Image.asset("images/mao.png"),
+          builder: (BuildContext context , child){
+            return Center(
+              child: Container(
+                height: animation.value,
+                width: animation.value,
+                child: child,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
+
   @override
   void dispose() {
     controller.dispose();
@@ -52,16 +69,18 @@ class _BaseAnimationRouteState extends State<BaseAnimationRoute> with SingleTick
 // 用 AnimationWidget 简化
 
 class AnimationImage extends AnimatedWidget {
-  AnimationImage({Key? key ,required Animation<double>animation}) : super(key: key, listenable: animation);
+  AnimationImage({Key? key, required Animation<double> animation})
+      : super(key: key, listenable: animation);
+
   @override
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
     return Center(
-      child: Image.asset("images/mao.png",
-      width: animation.value,
-      height: animation.value,),
+      child: Image.asset(
+        "images/mao.png",
+        width: animation.value,
+        height: animation.value,
+      ),
     );
-
   }
-
 }
